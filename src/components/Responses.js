7c602cab2bc46fee331e16bbca0e535d6335e3c4
LM <lm5dc@virginia.edu>
2022-02-27 19:24:19 -0500

    created a responses page

---
+++ src/components/Responses.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Responses(props) {
+
+    props.setPath("/responses");
+
+        return (
+            <div className="container">
+                <h3 className="mt-3">Responses</h3>
+            </div>
+        )
+    
+}
