34a8daaa123520018608bf688d87267c60fff50d
Cyue19 <yue_cheryl@hotmail.com>
2022-03-17 12:13:43 -0400

    made graphs and recent upload side by side

--- src/components/Responses.js
+++ src/components/Responses.js
@@ -16,12 +16,12 @@ export default function Responses(props) {
                     <h5 className="mt-3">Pain Responses</h5>
 
                     <div className="row">
-                        <div className="col-9 p-0">
-                            <div className="form-card">
+                        <div className="col-md-9" style={{padding: "0px 15px"}}>
+                            <div className="form-card" style={{margin: "0", width: "55vw"}}>
                                 <BarChart type="pain" endPoint="/responses/pain/counts" />
                             </div>
                         </div>
-                        <div className="col-3">
+                        <div className="col-md-3">
                             <div className="form-card">
                                 <b>Last Uploaded:</b>
                                 <LastUpload type="pain" endPoint="/responses/pain/last"/>
@@ -32,33 +32,37 @@ export default function Responses(props) {
                 </div>
                 <div>
                     <h5 className="mt-3">Follow-up Responses</h5>
-                    <div className="col-9" style={{padding: "0px 15px"}}>
-                        <div className="form-card my-3" style={{margin: "0", width: "55vw"}}>
-                            {/* <FollowUpQ1Chart/> */}
-                            <BarChart type="followUp" endPoint="/responses/follow_up/q1" />
-                        </div>
-                    </div>
-                    <div className="col-3">
-                            <div className="form-card mt-3">
-                            <b>Last Uploaded:</b>
-                            {/* <FollowUpRecent/> */}
-                            <LastUpload type="followUp" endPoint="/responses/follow_up/last" />
+                    <div className="row">
+                        <div className="col-9" style={{padding: "0px 15px"}}>
+                            <div className="form-card my-3" style={{margin: "0", width: "55vw"}}>
+                                {/* <FollowUpQ1Chart/> */}
+                                <BarChart type="followUp" endPoint="/responses/follow_up/q1" />
                             </div>
+                        </div>
+                        <div className="col-3">
+                                <div className="form-card mt-3">
+                                <b>Last Uploaded:</b>
+                                {/* <FollowUpRecent/> */}
+                                <LastUpload type="followUp" endPoint="/responses/follow_up/last" />
+                                </div>
+                        </div>
                     </div>
                 </div>
                 <div>
                     <h5 className="mt-3">End-of-day Responses</h5>
-                    <div className="col-9" style={{padding: "0px 15px"}}>
-                        <div className="form-card my-3" style={{margin: "0", width: "55vw"}}>
-                        {/* <EndOfDayQ1Chart/> */}
-                            <BarChart type="eod" endPoint="/responses/end_of_day/q1" />
+                    <div className="row">
+                        <div className="col-9" style={{padding: "0px 15px"}}>
+                            <div className="form-card my-3" style={{margin: "0", width: "55vw"}}>
+                            {/* <EndOfDayQ1Chart/> */}
+                                <BarChart type="eod" endPoint="/responses/end_of_day/q1" />
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-3">
-                        <div className="form-card mt-3">
-                            <b>Last Uploaded:</b>
-                            {/* <EndOfDayRecent/> */}
-                            <LastUpload type="eod" endPoint="/responses/end_of_day/last" />
+                        <div className="col-3">
+                            <div className="form-card mt-3">
+                                <b>Last Uploaded:</b>
+                                {/* <EndOfDayRecent/> */}
+                                <LastUpload type="eod" endPoint="/responses/end_of_day/last" />
+                            </div>
                         </div>
                     </div>
                 </div>
