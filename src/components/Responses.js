db243e3995d4d1c17e3dbf2838e8a58d483df88f
Cyue19 <yue_cheryl@hotmail.com>
2022-03-17 12:16:43 -0400

    cleaned up code

--- src/components/Responses.js
+++ src/components/Responses.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import FollowUpRecent from "./FollowUpRecent";
-import FollowUpQ1Chart from "./FollowUpQ1";
-import EndOfDayRecent from "./EndOfDayRecent";
-import EndOfDayQ1Chart from "./EndOfDayQ1";
 import LastUpload from "./LastUpload";
 import BarChart from "./BarChart";
 export default function Responses(props) {
@@ -21,6 +17,7 @@ export default function Responses(props) {
                                 <BarChart type="pain" endPoint="/responses/pain/counts" />
                             </div>
                         </div>
+                        
                         <div className="col-md-3">
                             <div className="form-card">
                                 <b>Last Uploaded:</b>
@@ -32,35 +29,35 @@ export default function Responses(props) {
                 </div>
                 <div>
                     <h5 className="mt-3">Follow-up Responses</h5>
+
                     <div className="row">
                         <div className="col-9" style={{padding: "0px 15px"}}>
-                            <div className="form-card my-3" style={{margin: "0", width: "55vw"}}>
-                                {/* <FollowUpQ1Chart/> */}
+                            <div className="form-card" style={{margin: "0", width: "55vw"}}>
                                 <BarChart type="followUp" endPoint="/responses/follow_up/q1" />
                             </div>
                         </div>
+
                         <div className="col-3">
-                                <div className="form-card mt-3">
+                            <div className="form-card mt-3">
                                 <b>Last Uploaded:</b>
-                                {/* <FollowUpRecent/> */}
                                 <LastUpload type="followUp" endPoint="/responses/follow_up/last" />
-                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div>
                     <h5 className="mt-3">End-of-day Responses</h5>
+
                     <div className="row">
                         <div className="col-9" style={{padding: "0px 15px"}}>
-                            <div className="form-card my-3" style={{margin: "0", width: "55vw"}}>
-                            {/* <EndOfDayQ1Chart/> */}
+                            <div className="form-card" style={{margin: "0", width: "55vw"}}>
                                 <BarChart type="eod" endPoint="/responses/end_of_day/q1" />
                             </div>
                         </div>
+
                         <div className="col-3">
                             <div className="form-card mt-3">
                                 <b>Last Uploaded:</b>
-                                {/* <EndOfDayRecent/> */}
                                 <LastUpload type="eod" endPoint="/responses/end_of_day/last" />
                             </div>
                         </div>
